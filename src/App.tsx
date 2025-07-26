240f0cc237bf99d5faed579ef5b4722c60935f77
Daniil Pereskokov <129772326+Daniil8565@users.noreply.github.com>
2025-07-26 18:16:40 +0300

    Закончена работа по 3 спринту

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
+  useLocation,
+  useNavigate,
+  Location,
 } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
 import AppHeader from './components/AppHeader/AppHeader';
 import SigninPage from './pages/SigninPage/SigninPage';
 import CustomBurger from './pages/CustomBurger/CustomBurger';
@@ -12,19 +17,33 @@ import SignupPage from './pages/SignupPage/SignupPage';
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 import ResetPassword from './pages/ResetPassword/ResetPassword';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import IngredientPage from './pages/IngredientPage/IngredientPage';
+import Modal from './components/Modal/Modal';
+import IngredientDetails from './components/IngredientDetails/IngredientDetails';
+
 import { useTypedSelector } from './hooks/useTypedSelector';
 import ProtectedRouteElement from './components/ProtectedRouteElement/ProtectedRouteElement';
 import { RootState } from './services/reducers/index';
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const success = useTypedSelector(
     (state: RootState) => state.forgotPasswordReducer.success
   );
+  const selectedItem = useTypedSelector(
+    (state: RootState) => state.IngredientDetailsReducer.selectedIngredient
+  );
+
+  // Приводим тип location.state к объекту с возможным полем background (если есть)
+  const state = location.state as { background?: Location };
 
   return (
-    <Router>
+    <>
       <AppHeader />
-      <Routes>
+      <Routes location={state?.background || location}>
         {/* Публичный маршрут */}
         <Route path="/" element={<CustomBurger />} />
 
@@ -53,7 +72,6 @@ function App() {
             </ProtectedRouteElement>
           }
         />
-        {/* <Route path="/reset-password" element={<ResetPassword />} /> */}
         <Route
           path="/reset-password"
           element={
@@ -76,8 +94,29 @@ function App() {
             </ProtectedRouteElement>
           }
         />
+        <Route path="/ingredients/:id" element={<IngredientPage />} />
       </Routes>
-    </Router>
+
+      {/* Модалка поверх главной страницы */}
+      {state?.background && selectedItem && (
+        <Routes>
+          <Route
+            path="/ingredients/:id"
+            element={
+              <Modal
+                onClick={() => {
+                  navigate(-1);
+                  dispatch({ type: 'CLEAR_INGREDIENT_DETAILS' });
+                }}
+                header="Детали ингредиента"
+              >
+                <IngredientDetails item={selectedItem} />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </>
   );
 }
 
